8a6077cab5e4dc21c8f577684e25ab6979f7462b
Atish Narasinha Dixit <ad181@pet.hw.ac.uk>
2019-06-04 17:37:18 +0100

    record rewards

--- cliff_Qalgo.py
+++ cliff_Qalgo.py
@@ -6,18 +6,24 @@ if __name__ == '__main__':
     gamma = 0.9
     epsilon = 0.1
     alpha = 0.8
+
+    r_Q = []
+
     for episodes in range(100):
         statesSet = [[0,4]]
         agent = Agent([0,4])
+        r_Q_e = []
         print(episodes)
         while True:
             currentState = list(agent.state)
             currentAction = env.policy(agent.state, gamma, epsilon )
             reward = agent.take_action(env, currentAction)
+            r_Q_e.append(reward)
             bestAction = env.greedy_action(agent.state)
             env.q_table[currentState[0], currentState[1], env.actionSet.index(currentAction) ] += alpha * (reward + gamma*env.q_table [ agent.state[0], agent.state[1], env.actionSet.index(bestAction) ] - env.q_table [ currentState[0], currentState[1], env.actionSet.index(currentAction) ])
             statesSet.append(list(agent.state))
             if agent.state == env.terminal_state or env.cliff_state.count(agent.state):
                 break
+        r_Q.append(np.mean(r_Q_e))
         print(statesSet)
         env.show(statesSet)
--- cliff_SARSA.py
+++ cliff_SARSA.py
@@ -6,21 +6,27 @@ if __name__ == '__main__':
     gamma = 0.9
     epsilon = 0.1
     alpha = 0.8
+
+    r_SARSA = []
+
     for episodes in range(100):
         statesSet = [[0,4]]
         agent = Agent([0,4])
-        # currentAction = env.policy(agent.state, gamma, epsilon )
-        currentAction = env.policy(agent.state, gamma, 1/(1+2*episodes) )
+        currentAction = env.policy(agent.state, gamma, epsilon )
+        # currentAction = env.policy(agent.state, gamma, 1/(1+2*episodes) )
+        r_SARSA_e = []
         print(episodes)
         while True:
             currentState = list(agent.state)
             reward = agent.take_action(env, currentAction)
-            # NextAction = env.policy(agent.state, gamma, epsilon )
-            NextAction = env.policy(agent.state, gamma, 1/(1+2*episodes) )
+            r_SARSA_e.append(reward)
+            NextAction = env.policy(agent.state, gamma, epsilon )
+            # NextAction = env.policy(agent.state, gamma, 1/(1+2*episodes) )
             env.q_table[currentState[0], currentState[1], env.actionSet.index(currentAction) ] += alpha * (reward + gamma*env.q_table [ agent.state[0], agent.state[1], env.actionSet.index(NextAction) ] - env.q_table [ currentState[0], currentState[1], env.actionSet.index(currentAction) ])
             statesSet.append(list(agent.state))
             currentAction = NextAction
             if agent.state == env.terminal_state or env.cliff_state.count(agent.state):
                 break
+        r_SARSA.append(np.mean(r_SARSA_e))
         print(statesSet)
         env.show(statesSet)
