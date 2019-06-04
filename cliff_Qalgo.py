2c7f2d088b7d46f4a180483013a7286d60640567
Atish Narasinha Dixit <ad181@pet.hw.ac.uk>
2019-06-04 16:31:13 +0100

    final edit

--- cliff_Qalgo.py
+++ cliff_Qalgo.py
@@ -1,4 +1,5 @@
 import numpy as np
+import copy
 
 class Agent:
     def __init__(self, state):
@@ -23,7 +24,7 @@ class Agent:
         else:
             reward = -1
 
-        return self.state, reward
+        return reward
 
 
 class Environment:
@@ -67,17 +68,18 @@ class Environment:
 if __name__ == '__main__':
     env = Environment()
     gamma = 0.9
-    epsilon = 0.2
+    epsilon = 0.1
     alpha = 0.8
     for episodes in range(100):
         statesSet = [[0,4]]
         agent = Agent([0,4])
         print(episodes)
         while True:
-            nextAction = env.policy(agent.state, gamma, epsilon)
-            agent.state , reward = agent.take_action(env, nextAction)
+            currentState = list(agent.state)
+            nextAction = env.policy(agent.state, gamma, epsilon )
+            reward = agent.take_action(env, nextAction)
             bestAction = env.greedy_action(agent.state)
-            env.q_table[agent.state[0], agent.state[1], env.actionSet.index(nextAction) ] += alpha * (reward + gamma*env.q_table [ agent.state[0], agent.state[1], env.actionSet.index(bestAction) ] - env.q_table [ agent.state[0], agent.state[1], env.actionSet.index(nextAction) ])
+            env.q_table[currentState[0], currentState[1], env.actionSet.index(nextAction) ] += alpha * (reward + gamma*env.q_table [ agent.state[0], agent.state[1], env.actionSet.index(bestAction) ] - env.q_table [ currentState[0], currentState[1], env.actionSet.index(nextAction) ])
             statesSet.append(list(agent.state))
             if agent.state == env.terminal_state or env.cliff_state.count(agent.state):
                 break
